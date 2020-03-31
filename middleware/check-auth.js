73a486f8b808ff339d94e3209a2297633229788f
Colucci, Emanuele <emanuele.colucci@accenture.com>
2020-03-31 16:40:24 +0200

    removeEnv

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(" ")[1];
-        jwt.verify(token, process.env.JWT_KEY);
+        jwt.verify(token, "secret_test_token");
         next();
     } catch(error){
         res.status(401).json({message: 'Not Authenticated!'});
